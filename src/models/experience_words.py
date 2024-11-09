c9b584c225290f99ff4f92244f90873a224f1f66
ukubach <ukubach.making@gmx.de>
2024-11-09 18:46:08 +0100

    Added texts for the experience by words analysis and changed the way experience is quantified. Also added some comments here, refacrored some stuff there, you know how it goes.

--- src/models/experience_words.py
+++ src/models/experience_words.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
 # these are some possibilities of what one could consider
 # "word that only experienced beer consumers would use in there beer review"
@@ -76,6 +77,80 @@ def get_experienced_users(df_ratings, exp_words):
     ]
     exp_user_ids = df_ratings_exp["user_id"].unique()
     return exp_user_ids
+
+
+def get_experienced_users2(df_ratings, exp_words):
+    """
+    This is a second way to define experienced users using the words they use.
+    Here we give two criteria that need to be satisfied in order to call someone experienced.
+    First of all they need to use at least 5 of the word in the exp_words list.
+    Secondly, they need to use one of the words in at least ten distinct ratings.
+    The method itself uses the 3 sub-methods filter_ratings_with_exp_words, get_users_with_min_exp_words
+    and filter_experienced_users.
+    :param df_ratings: the rating dataset
+    :param exp_words: the list of words we consider to come from experienced users
+    :return: a list of ids of experienced users
+    """
+    ratings_with_exp_words = filter_ratings_with_exp_words(df_ratings, exp_words)
+    users_with_exp_words = get_users_with_min_exp_words(
+        ratings_with_exp_words, exp_words
+    )
+    exp_users = filter_experienced_users(ratings_with_exp_words, users_with_exp_words)
+    return exp_users
+
+
+def filter_ratings_with_exp_words(df_ratings, exp_words):
+    """
+    This filters the dataframe only to those entries that include at least one of the given words.
+    :param df_ratings: the rating df
+    :param exp_words: the list of words we consider to come from experienced users
+    :return: filtered df
+    """
+    regex_pattern = "|".join(exp_words)
+    return df_ratings[
+        df_ratings["text"].str.contains(regex_pattern, case=False, na=False)
+    ]
+
+
+def get_users_with_min_exp_words(df_ratings_exp, exp_words, min_word_count=5):
+    """
+    This implements the criterion of at least 5(/min_word_count) exp_words used
+    :param df_ratings_exp: the rating df
+    :param exp_words: the list of words we consider to come from experienced users
+    :param min_word_count: the threshold how many unique exp_words an exp. user must use
+    :return:
+    """
+    regex_pattern = "|".join(exp_words)
+    word_counts = (
+        df_ratings_exp.assign(
+            word_matches=df_ratings_exp["text"].str.findall(
+                regex_pattern, flags=re.IGNORECASE
+            )
+        )
+        .explode("word_matches")
+        .groupby(["user_id", "word_matches"])
+        .size()
+        .unstack(fill_value=0)
+    )
+    return word_counts[word_counts.gt(0).sum(axis=1) >= min_word_count].index
+
+
+def filter_experienced_users(df_ratings_exp, experienced_user_ids, min_ratings=10):
+    """
+    This implements the criterion that an experienced user must use exp_words in at least 10(/min_ratings) many ratings
+    :param df_ratings_exp: dataset of ratings
+    :param experienced_user_ids: result of get_users_with_min_exp_words
+    :param min_ratings: threshold how many  ratings with an exp_words there
+     must be for a user to be considered experienced
+    :return:
+    """
+    return (
+        df_ratings_exp[df_ratings_exp["user_id"].isin(experienced_user_ids)]
+        .groupby("user_id")
+        .size()
+        .loc[lambda x: x >= min_ratings]
+        .index.to_numpy()
+    )
 
 
 def split_by_experience(df_ratings, exp_user_ids):
