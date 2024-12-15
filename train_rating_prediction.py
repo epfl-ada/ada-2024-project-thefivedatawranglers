7903336888d8101ff92ab6803ea33152d9307479
Benedikt Edler von Querfurth <querfurth@izar1.izar.cluster>
2024-12-15 16:32:44 +0100

    Added foreign bias into NN

--- train_rating_prediction.py
+++ train_rating_prediction.py
@@ -4,23 +4,21 @@ import numpy as np
 import pandas as pd
 from glob import glob
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
-from torchsummary import summary
 
 # %%
-df = pd.read_csv("data/RatingPrediction/rating_reviewer_pairs.csv")
+df = pd.read_csv("data/RatingPrediction/rating_reviewer_pairs_foreign.csv")
 
 # %%
 df.dropna(inplace=True)
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
-print(df_train.shape, df_test.shape)
+# df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+# print(df_train.shape, df_test.shape)
 
 scaler = StandardScaler()
-df_train = scaler.fit_transform(df_train)
-df_test = scaler.transform(df_test)
+df = scaler.fit_transform(df)
 
 # df_train[:, 9:317] = 0  # No text
 # df_test[:, 9:317] = 0  # No text
@@ -75,7 +73,7 @@ class RatingModel(torch.nn.Module):
         self.embd_good_word_beer = torch.nn.Linear(77, 15)
         self.embd_bad_word_beer = torch.nn.Linear(77, 15)
 
-        self.first_layer = torch.nn.Linear(94, 52)
+        self.first_layer = torch.nn.Linear(95, 52)
         self.second_layer = torch.nn.Linear(52, 35)
         self.third_layer = torch.nn.Linear(35, 35)
         self.fourth_layer = torch.nn.Linear(35, 35)
@@ -86,15 +84,15 @@ class RatingModel(torch.nn.Module):
         self.dropout = torch.nn.Dropout(0.1)
 
     def forward(self, x):
-        classical_biases = x[:, 1:9]
-        user_good_words = x[:, 9:86]
-        user_bad_words = x[:, 86:163]
-        beer_good_words = x[:, 163:240]
-        beer_bad_words = x[:, 240:317]
-        beer_cat = x[:, 317:330]
-        month = x[:, 330:342]
-        foreign_us_aggr = x[:, 342]
-        foreign_us_split = x[:, 343]
+        classical_biases = x[:, :8]
+        user_good_words = x[:, 8:85]
+        user_bad_words = x[:, 85:162]
+        beer_good_words = x[:, 162:239]
+        beer_bad_words = x[:, 239:316]
+        beer_cat = x[:, 316:329]
+        month = x[:, 329:341]
+        foreign_us_aggr = x[:, 341:342]
+        foreign_us_split = x[:, 342:343]
 
         user_good_words = self.relu(self.embd_good_word_user(user_good_words))
         user_bad_words = self.relu(self.embd_bad_word_user(user_bad_words))
@@ -125,94 +123,123 @@ class RatingModel(torch.nn.Module):
         return x
 
 
-# %%
-train_ds = torch.utils.data.TensorDataset(
-    torch.tensor(df_train[:, 1:], dtype=torch.float32),
-    torch.tensor(df_train[:, 0] > df_train[:, 1], dtype=torch.float32),
-)
-test_ds = torch.utils.data.TensorDataset(
-    torch.tensor(df_test[:, 1:], dtype=torch.float32),
-    torch.tensor(df_test[:, 0] > df_test[:, 1], dtype=torch.float32),
-)
+fold_accs = []
 
-train_loader = torch.utils.data.DataLoader(
-    train_ds, batch_size=256, shuffle=True, num_workers=8, persistent_workers=True
-)
-test_loader = torch.utils.data.DataLoader(
-    test_ds, batch_size=256, shuffle=False, num_workers=8, persistent_workers=True
-)
+for fold, (train_idx, test_idx) in enumerate(
+    KFold(n_splits=5, shuffle=True, random_state=42).split(df)
+):
+    df_train = df[train_idx]
+    df_test = df[test_idx]
 
-# %%
-model = RatingModel()
-crit = torch.nn.MSELoss()
-optim = torch.optim.AdamW(model.parameters(), lr=5e-4)
-
-# %%
-
-# print(summary(model, (1, 342)))
-model.cuda()
-losses_train = []
-losses_test = []
-accs_train = []
-accs_test = []
-
-best_acc_test = 0
-for epoch in tqdm(range(100)):
-    print(f"Epoch {epoch}")
-    model.train()
-    loss_train = []
-    acc_train = []
-
-    for x, y in tqdm(train_loader):
-        optim.zero_grad()
-        # model expects: classical_biases, user_good_words, user_bad_words, beer_good_words, beer_bad_words, beer_cat, month
-        y_pred = model(x.cuda())
-        y.cuda()
-        loss = crit(y_pred, y.unsqueeze(1).cuda())
-        loss.backward()
-        optim.step()
-        loss_train.append(loss.item())
-        acc_train.append(
-            accuracy_score(
-                y.cpu().detach().numpy(), y_pred.cpu().detach().numpy() > 0.5
-            )
-        )
-
-    model.eval()
-    loss_test = []
-    acc_test = []
-    with torch.no_grad():
-        for x, y in tqdm(test_loader):
-            y_pred = model(x.cuda())
-            loss = crit(y_pred, y.unsqueeze(1).cuda())
-            loss_test.append(loss.item())
-            acc_test.append(accuracy_score(y.cpu().numpy(), y_pred.cpu().numpy() > 0.5))
-
-    if best_acc_test < np.mean(acc_test):
-        best_acc_test = np.mean(acc_test)
-        torch.save(model.state_dict(), "best_model.pth")
-
-    # plt.plot(loss_train)
-    # plt.plot(loss_test)
-    # plt.show()
-    losses_train.append(np.mean(loss_train))
-    losses_test.append(np.mean(loss_test))
-    accs_train.append(np.mean(acc_train))
-    accs_test.append(np.mean(acc_test))
-    print(
-        f"Train loss: {np.mean(loss_train)}, Test loss: {np.mean(loss_test)}, Train acc: {np.mean(acc_train)}, Test acc: {np.mean(acc_test)}"
+    # %%
+    train_ds = torch.utils.data.TensorDataset(
+        torch.tensor(df_train[:, 1:], dtype=torch.float32),
+        torch.tensor(df_train[:, 0] > df_train[:, 1], dtype=torch.float32),
+    )
+    test_ds = torch.utils.data.TensorDataset(
+        torch.tensor(df_test[:, 1:], dtype=torch.float32),
+        torch.tensor(df_test[:, 0] > df_test[:, 1], dtype=torch.float32),
     )
 
+    train_loader = torch.utils.data.DataLoader(
+        train_ds, batch_size=256, shuffle=True, num_workers=8, persistent_workers=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_ds, batch_size=256, shuffle=False, num_workers=8, persistent_workers=True
+    )
 
-plt.plot(losses_train, label="train")
-plt.plot(losses_test, label="test")
-plt.legend()
-plt.title("Losses")
-plt.savefig("losses_relu_embd_15.png")
+    # %%
 
-plt.figure()
-plt.plot(accs_train, label="train")
-plt.plot(accs_test, label="test")
-plt.legend()
-plt.title("Accuracies")
-plt.savefig("accs_relu_embd_15.png")
+    model = RatingModel()
+    crit = torch.nn.MSELoss()
+    optim = torch.optim.AdamW(model.parameters(), lr=1e-3)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optim, "min", patience=10, factor=0.5
+    )
+
+    # %%
+
+    model.cuda()
+    losses_train = []
+    losses_test = []
+    accs_train = []
+    accs_test = []
+
+    best_acc_test = 0
+    epochs_no_improvement = 0
+    patience = 30
+    best_val_loss = np.inf
+    for epoch in tqdm(range(100)):
+        print(f"Epoch {epoch}")
+        model.train()
+        loss_train = []
+        acc_train = []
+
+        for x, y in tqdm(train_loader):
+            optim.zero_grad()
+            # model expects: classical_biases, user_good_words, user_bad_words, beer_good_words, beer_bad_words, beer_cat, month
+            y_pred = model(x.cuda())
+            y.cuda()
+            loss = crit(y_pred, y.unsqueeze(1).cuda())
+            loss.backward()
+            optim.step()
+            loss_train.append(loss.item())
+            acc_train.append(
+                accuracy_score(
+                    y.cpu().detach().numpy(), y_pred.cpu().detach().numpy() > 0.5
+                )
+            )
+
+        model.eval()
+        loss_test = []
+        acc_test = []
+        with torch.no_grad():
+            for x, y in tqdm(test_loader):
+                y_pred = model(x.cuda())
+                loss = crit(y_pred, y.unsqueeze(1).cuda())
+                loss_test.append(loss.item())
+                acc_test.append(
+                    accuracy_score(y.cpu().numpy(), y_pred.cpu().numpy() > 0.5)
+                )
+
+        val_loss = np.mean(loss_test)
+        scheduler.step(val_loss)
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            epochs_no_improvement = 0
+        else:
+            epochs_no_improvement += 1
+            if epochs_no_improvement > patience:
+                break
+
+        if best_acc_test < np.mean(acc_test):
+            best_acc_test = np.mean(acc_test)
+            torch.save(model.state_dict(), "best_model.pth")
+
+        # plt.plot(loss_train)
+        # plt.plot(loss_test)
+        # plt.show()
+        losses_train.append(np.mean(loss_train))
+        losses_test.append(np.mean(loss_test))
+        accs_train.append(np.mean(acc_train))
+        accs_test.append(np.mean(acc_test))
+        print(
+            f"Train loss: {np.mean(loss_train)}, Test loss: {np.mean(loss_test)}, Train acc: {np.mean(acc_train)}, Test acc: {np.mean(acc_test)}, LR: {optim.param_groups[0]['lr']}"
+        )
+    fold_accs.append(best_acc_test)
+
+    plt.plot(losses_train, label="train")
+    plt.plot(losses_test, label="test")
+    plt.legend()
+    plt.title("Losses")
+    plt.savefig("losses_relu_embd_15.png")
+
+    plt.figure()
+    plt.plot(accs_train, label="train")
+    plt.plot(accs_test, label="test")
+    plt.legend()
+    plt.title("Accuracies")
+    plt.savefig("accs_relu_embd_15.png")
+
+print(f"Fold accs: {fold_accs}")
+print(f"Mean acc: {np.mean(fold_accs)}")
